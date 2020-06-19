2481a73ef3a518a0aa3e74922e98b7533d87bb39
Agunahwan Absin <vair.agun@gmail.com>
2020-06-19 12:58:06 +0700

    Initial Version

---
+++ API1/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace API1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public void Index()
+        {
+            //ViewBag.Title = "Home Page";
+
+            //return View();
+            Response.Redirect("~/Swagger");
+        }
+    }
+}
---
+++ API1/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace API1.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        static HttpClient client = new HttpClient();
+
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            string[] data = new string[] { "Hello World from API 1 !" };
+            return data;
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public async Task<IEnumerable<string>> Post(string path = "")
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                path = "http://localhost/API2/api/Values";
+            }
+            IEnumerable<string> data = null;
+            HttpResponseMessage response = await client.PostAsJsonAsync(path, data);
+            if (response.IsSuccessStatusCode)
+            {
+                data = await response.Content.ReadAsAsync<IEnumerable<string>>();
+            }
+            return data;
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
